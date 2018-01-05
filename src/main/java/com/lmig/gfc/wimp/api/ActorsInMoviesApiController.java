8fd33ff3f412fd94fd674241c882a1c5fec256f3
David Hall <david.hall@libertymutual.com>
2018-01-05 17:08:55 -0500

    added cross origin support to movies in actors API

--- src/main/java/com/lmig/gfc/wimp/api/ActorsInMoviesApiController.java
+++ src/main/java/com/lmig/gfc/wimp/api/ActorsInMoviesApiController.java
@@ -1,5 +1,6 @@
 package com.lmig.gfc.wimp.api;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import com.lmig.gfc.wimp.services.MovieRepository;
 
 @RestController
 @RequestMapping("/api/movies/{movieId}/actors")
+@CrossOrigin(origins = "*")
 public class ActorsInMoviesApiController {
 
 	private MovieRepository mr;
